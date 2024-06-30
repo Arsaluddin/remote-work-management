e1db17b49433d20fd433a20bcf0a8a8605241e5b
arsal uddin <arsaluddin134@gmail.com>
2024-06-30 20:34:34 +0530

    role  based access

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
+const role = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.post('/', auth, createTask);
+router.post('/', auth, role(['Admin', 'Manager']), createTask);
 router.get('/', auth, getTasks);
-router.put('/:id', auth, updateTask);
-router.delete('/:id', auth, deleteTask);
+router.put('/:id', auth, role(['Admin', 'Manager']), updateTask);
+router.delete('/:id', auth, role(['Admin']), deleteTask);
 
 module.exports = router;
+
